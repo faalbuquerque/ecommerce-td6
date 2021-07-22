a65f66a84b29ede184f7fd462c42ab1a8cb910da
Mariana_C_Ruiz <mchierandaruiz0@gmail.com>
2021-07-22 15:23:10 -0300

    adicionando before action no shipping controller

--- app/controllers/shippings_controller.rb
+++ app/controllers/shippings_controller.rb
@@ -1,9 +1,8 @@
 class ShippingsController < ApplicationController
   before_action :find_product, only: %i[index]
+  before_action :find_address, only: %i[index]
 
   def index
-    find_params
-    find_adresses
     @shippings = Shipping.to_product(@product, shipping_params[:cep])
     render 'products/show'
   end
@@ -18,14 +17,7 @@ class ShippingsController < ApplicationController
     @product = Product.find(params[:product_id])
   end
 
-  def find_params
-    params[:sku] = @product.sku
-    params[:weight] = @product.weight
-    params[:length] = @product.length
-    params[:width] = @product.width
-  end
-
-  def find_adresses
+  def find_address
     @addresses = current_user.addresses if current_user
   end
 end
