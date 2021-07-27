7971ab75641d38a5535d2098d1428b6b00922365
Giovani Fernandes <giovani_oli@hotmail.com>
2021-07-27 09:59:46 -0400

    Adicionado css na home

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,7 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import * as bootstrap from "bootstrap"
+import 'bootstrap-icons/font/bootstrap-icons.css'
 
 Rails.start()
 Turbolinks.start()
