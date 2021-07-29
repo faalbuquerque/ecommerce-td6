77d4256bdccc0f0ac57f79b58dab31576b6ce333
Giovani Fernandes <giovani_oli@hotmail.com>
2021-07-29 16:30:52 -0400

    correção herança admin product controller

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,4 +1,4 @@
-class Admin::ProductsController < ApplicationController
+class Admin::ProductsController < Admin::AdminController
   before_action :find_product, only: %i[show edit update]
   before_action :all_products, only: %i[index new create edit update]
 
