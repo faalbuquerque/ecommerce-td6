be6583f40a57adc9851c5cd8a4e48122f4bb1a4a
Mariana_C_Ruiz <mchierandaruiz0@gmail.com>
2021-07-29 01:14:42 -0300

    autentificação de usuario post patch

--- spec/requests/user_authentication_spec.rb
+++ spec/requests/user_authentication_spec.rb
@@ -1,24 +1,24 @@
 require 'rails_helper'
 
-describe 'authenticating user' do
+RSpec.describe 'authenticating user', type: :request do
   context 'visitor' do
     context 'cart' do
-      xit 'POST' do
+      it 'POST' do
         post users_carts_path, params: { cart: { user_id: '1' } }
 
-        expect(response).to redirect_to(new_user_session_path)
+        expect(last_response.headers['location']).to be == 'http://example.org/users/sign_in'
       end
     end
     context 'evaluations' do
-      xit 'POST' do
+      it 'POST' do
         post users_evaluations_path, params: { cart: { user_id: 1 } }
 
-        expect(response).to redirect_to(new_user_session_path)
+        expect(last_response.headers['location']).to be == 'http://example.org/users/sign_in'
       end
-      xit 'PATCH' do
+      it 'PATCH' do
         patch users_evaluation_path(id: 1), params: { cart: { user_id: 1 } }
 
-        expect(response).to redirect_to(new_user_session_path)
+        expect(last_response.headers['location']).to be == 'http://example.org/users/sign_in'
       end
     end
   end
